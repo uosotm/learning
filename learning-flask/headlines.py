1e47b76b35e055edc67a4e59f39db5c59c46005c
Yuta Katayama <uosotm@gmail.com>
2018-02-08 01:38:12 +0900

    Add dynamic routing to get multiple feeds

--- learning-flask/headlines.py
+++ learning-flask/headlines.py
@@ -2,24 +2,27 @@ import feedparser
 from flask import Flask
 
 
-
 app = Flask(__name__)
-HN_FEED = "https://news.ycombinator.com/rss"
+RSS_FEEDS = {'hacker_news': 'https://news.ycombinator.com/rss',
+             'mozilla': 'https://blog.mozilla.org/feed/',
+             'make': 'https://makezine.com/feed/'}
 
 
 @app.route("/")
-def get_news():
-    feed = feedparser.parse(HN_FEED)
+@app.route("/<publication>")
+def get_news(publication='hacker_news'):
+    feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     return """<html>
         <body>
-            <h1>Hacker News</h1>
+            <h1>Headlines</h1>
             <b>{0}</b> <br/>
             <i>{1}</i> <br/>
             <p>{2}</p> <br/>
         </body>
     </html>""".format(first_article.get("title"), first_article.
             get("published"), first_article.get("summary"))
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
