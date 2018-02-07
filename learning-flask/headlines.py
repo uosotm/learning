a698f52c157773c84571dc8e7057bdea51317134
Yuta Katayama <uosotm@gmail.com>
2018-02-07 23:49:50 +0900

    Add feed parser to parse hacker news feed

---
+++ learning-flask/headlines.py
@@ -0,0 +1,25 @@
+import feedparser
+from flask import Flask
+
+
+
+app = Flask(__name__)
+HN_FEED = "https://news.ycombinator.com/rss"
+
+
+@app.route("/")
+def get_news():
+    feed = feedparser.parse(HN_FEED)
+    first_article = feed['entries'][0]
+    return """<html>
+        <body>
+            <h1>Hacker News</h1>
+            <b>{0}</b> <br/>
+            <i>{1}</i> <br/>
+            <p>{2}</p> <br/>
+        </body>
+    </html>""".format(first_article.get("title"), first_article.
+            get("published"), first_article.get("summary"))
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
