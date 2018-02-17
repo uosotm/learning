d940b560c5820c28622d88adbb70868e9677d05f
Yuta Katayama <uosotm@gmail.com>
2018-02-18 02:18:53 +0900

    Add request context to get pulication as a query

--- learning-flask/headlines/headlines.py
+++ learning-flask/headlines/headlines.py
@@ -1,6 +1,7 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
 
 
 app = Flask(__name__)
@@ -10,8 +11,13 @@ RSS_FEEDS = {'hacker_news': 'https://news.ycombinator.com/rss',
 
 
 @app.route("/")
-@app.route("/<publication>")
 def get_news(publication='hacker_news'):
+    query = request.args.get('publication')
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = 'hacker_news'
+    else:
+        publication = query.lower()
+
     feed = feedparser.parse(RSS_FEEDS[publication])
     return render_template("home.html", articles=feed['entries'])
 
