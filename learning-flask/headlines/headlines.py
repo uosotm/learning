31bb14317b2a361e0fb522b61db823925ac2a567
Yuta Katayama <uosotm@gmail.com>
2018-02-20 08:57:03 +0900

    Add ability to show current weather

--- learning-flask/headlines/headlines.py
+++ learning-flask/headlines/headlines.py
@@ -1,14 +1,36 @@
 import feedparser
+import json
+import os
+import urllib
+
 from flask import Flask
 from flask import render_template
 from flask import request
 
 
 app = Flask(__name__)
+
 RSS_FEEDS = {'hacker_news': 'https://news.ycombinator.com/rss',
              'mozilla': 'https://blog.mozilla.org/feed/',
              'make': 'https://makezine.com/feed/'}
+# WEATHER_URL = ("http://api.openweathermap.org/data/2.5/weather?q={}\
+#                &units=metric&APPID=%s" % os.environ['OWM_API_KEY'])
 
+
+def get_weather(query):
+    api_key = os.environ['OWM_API_KEY']
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}"
+
+    query = urllib.parse.quote(query)
+    url = api_url.format(query, api_key)
+    data = urllib.request.urlopen(url).read()
+    parsed = json.loads(data.decode('utf-8'))
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description": parsed["weather"][0]["description"],
+                   "temperature": parsed["main"]["temp"],
+                   "city": parsed["name"]}
+    return weather
 
 @app.route("/")
 def get_news(publication='hacker_news'):
@@ -19,7 +41,10 @@ def get_news(publication='hacker_news'):
         publication = query.lower()
 
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles=feed['entries'])
+    weather = get_weather("London,UK")
+    return render_template("home.html",
+                           articles=feed['entries'],
+                           weather=weather)
 
 
 if __name__ == '__main__':
