30b6a6ae877e5728784bf84915fc2bafc221b49c
Yuta Katayama <uosotm@gmail.com>
2018-03-03 21:23:31 +0900

    Accepts more currencies with for looping

--- learning-flask/headlines/headlines.py
+++ learning-flask/headlines/headlines.py
@@ -41,14 +41,15 @@ def home():
     currency_to = request.args.get('currency_to')
     if not currency_to:
         currency_to = DEFAULTS['currency_to']
-    rate = get_rate(currency_from, currency_to)
+    rate, currencies = get_rate(currency_from, currency_to)
     
     return render_template('home.html',
                            articles=articles,
                            weather=weather,
                            currency_from=currency_from,
                            currency_to=currency_to,
-                           rate=rate)
+                           rate=rate,
+                           currencies=sorted(currencies))
 
 def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
@@ -79,7 +80,7 @@ def get_rate(frm, to):
     parsed = json.loads(all_currency.decode('utf-8')).get('rates')
     frm_rate = parsed.get(frm.upper())
     to_rate = parsed.get(to.upper())
-    return to_rate/frm_rate
+    return (to_rate / frm_rate, parsed.keys())
 
 
 if __name__ == '__main__':
